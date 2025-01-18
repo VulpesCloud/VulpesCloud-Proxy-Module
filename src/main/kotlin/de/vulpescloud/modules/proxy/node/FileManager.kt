d9137a85edf665bd27add34ba2b7145b7c3713bb
TheCGuyGitHub <christimaibach1@gmail.com>
2025-01-19 00:31:55 +0100

    fix: Module copy's itself now

--- src/main/kotlin/de/vulpescloud/modules/proxy/node/FileManager.kt
+++ src/main/kotlin/de/vulpescloud/modules/proxy/node/FileManager.kt
@@ -1,15 +1,33 @@
 package de.vulpescloud.modules.proxy.node
 
 import de.vulpescloud.api.services.Service
+import de.vulpescloud.node.Node
+import de.vulpescloud.node.services.LocalServiceImpl
+import org.slf4j.LoggerFactory
+import java.nio.file.Files
+import kotlin.io.path.Path
 
 object FileManager {
+    val jarPath = Path("modules/VulpesCloud-Proxy-Module-1.0-SNAPSHOT.jar")
+    val configPath = Path("modules/VulpesCloud-Proxy-Module/config.json")
+    private val logger = LoggerFactory.getLogger(FileManager::class.java)
 
     fun copyModuleIntoService(service: Service) {
+        if (service is LocalServiceImpl) {
+            val pluginDir = service.runningDir.resolve("plugins")
+            Files.copy(jarPath, pluginDir.resolve("VulpesCloud-Proxy-Module.jar"))
 
+            logger.debug("Copying into {}", pluginDir)
+        }
     }
 
     fun copyConfigIntoService(service: Service) {
-
+        if (service is LocalServiceImpl) {
+            val pluginDir = service.runningDir.resolve("plugins").resolve("VulpesCloud-Proxy-Module")
+            pluginDir.toFile().mkdirs()
+            Files.copy(configPath, pluginDir.resolve("config.json"))
+            logger.debug("Copying config into {}", pluginDir)
+        }
     }
 
 }
