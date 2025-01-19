60930be76f5ef84bca47c87ec9f6125e2b3889ae
TheCGuyGitHub <christimaibach1@gmail.com>
2025-01-19 13:10:04 +0100

    Motd now supports placeholders

--- src/main/kotlin/de/vulpescloud/modules/proxy/velocity/manager/MotdManager.kt
+++ src/main/kotlin/de/vulpescloud/modules/proxy/velocity/manager/MotdManager.kt
@@ -5,6 +5,7 @@ import com.velocitypowered.api.event.Subscribe
 import com.velocitypowered.api.event.proxy.ProxyPingEvent
 import com.velocitypowered.api.plugin.PluginContainer
 import com.velocitypowered.api.proxy.ProxyServer
+import de.vulpescloud.wrapper.Wrapper
 import net.kyori.adventure.text.minimessage.MiniMessage
 import org.json.JSONObject
 import kotlin.jvm.optionals.getOrNull
@@ -19,8 +20,19 @@ class MotdManager(
     @Subscribe
     fun onProxyPingEvent(event: ProxyPingEvent) {
         val motdJson = configJSON.getJSONObject("motd")
+
+        val fistLine = motdJson.getString("firstLine")
+            .replace("%proxy%", Wrapper.instance.service.name)
+            .replace("%playerCount%", "${event.ping.players.getOrNull()?.online ?: 0}")
+            .replace("%playerMax%", "${event.ping.players.getOrNull()?.max ?: 0}")
+
+        val secondLine = motdJson.getString("secondLine")
+            .replace("%proxy%", Wrapper.instance.service.name)
+            .replace("%playerCount%", "${event.ping.players.getOrNull()?.online ?: 0}")
+            .replace("%playerMax%", "${event.ping.players.getOrNull()?.max ?: 0}")
+
         val samplePlayers = event.ping.players.getOrNull()?.sample ?: emptyList()
-        val motd = MiniMessage.miniMessage().deserialize("${motdJson.getString("firstLine")}\n${motdJson.getString("secondLine")}")
+        val motd = MiniMessage.miniMessage().deserialize("$fistLine\n$secondLine")
         val builder = event.ping.asBuilder()
             .version(event.ping.version)
             .onlinePlayers(event.ping.players.getOrNull()?.online ?: 0)
